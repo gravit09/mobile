175a4f2c68ddfec7992ebaf97a6e414c0fab9f11
Gravit09 <garvitchoudhary09@gmail.com>
2024-05-27 23:03:40 +0530

    ch

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { ImageBackground, Pressable, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import image from "../assets/screenshot_2.png";
 
 export default function App() {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">First App!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to Profile
-      </Link>
+    <View className="flex-1">
+      <ImageBackground className="flex-1 justify-end" source={image} />
+      <View className="w-full bg-white p-5 items-start">
+        <Text className="text-3xl mb-4 font-bold">Get Started With Taxi</Text>
+        <Pressable
+          className="w-full font-semibold rounded-lg bg-black h-20 items-center"
+          onPress={() => router.push("/profile")}
+        >
+          <Text className="text-white text-3xl mt-5">Continue ➲</Text>
+        </Pressable>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
