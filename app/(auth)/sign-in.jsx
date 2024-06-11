9fdc5ef335b1450f1a47583394bfdf6dc42edeef
Gravit09 <garvitchoudhary09@gmail.com>
2024-06-12 01:51:02 +0530

    safe area issue fixed

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,4 +1,5 @@
-import { View, Text, SafeAreaView, ScrollView, TextInput } from "react-native";
+import { View, Text, ScrollView, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import { useRouter } from "expo-router";
 import SignButton from "../components/SignButton";
